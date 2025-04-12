5ec5d29d997f6672ca090716ac535234632bf1ec
Pacelin <Warfacer2020@gmail.com>
2025-04-12 19:45:48 +0300

    fix: EditorClass

--- Assets/_Project/Scripts/Game/Drones/DroneStationComponent.cs
+++ Assets/_Project/Scripts/Game/Drones/DroneStationComponent.cs
@@ -1,8 +1,23 @@
 ﻿using R3;
+using TSS.Tweening;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace GGJam25.Game.Drones
 {
+    public class DroneStart : MonoBehaviour, IPointerClickHandler
+    {
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            
+        }
+    }
+    
+    public class DroneStationPaths : MonoBehaviour
+    {
+        //[SerializeField] private 
+    }
+    
     public class DroneStationComponent : MonoBehaviour
     {
         public ReadOnlyReactiveProperty<DroneComponent> ActiveDrone => _activeDrone;
