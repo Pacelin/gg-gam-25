14acfba749c364992bac15e0098d1082a0929520
Pacelin <Warfacer2020@gmail.com>
2025-04-12 23:29:40 +0300

    feat: A lot

---
+++ Assets/_Project/Scripts/Game/Drones/DroneRadar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace GGJam25.Game.Indicators
+{
+    public class DroneRadar : MonoBehaviour
+    {
+        [SerializeField] private GameObject _enabledState;
+        [SerializeField] private GameObject _disabledState;
+        [SerializeField] private Transform _arrowHub;
+        [SerializeField] private Transform _arrowKey;
+
+        private void OnEnable()
+        {
+        }
+
+        private void OnDisable()
+        {
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/Game/Drones/Upgrades/DroneStabilizationUpgrade.cs
@@ -0,0 +1,25 @@
+﻿using TSS.Utils;
+using UnityEngine;
+
+namespace GGJam25.Game.Drones
+{
+    [CreateSingletonAsset("Assets/_Project/Configs/Upgrades/SO_DroneStabilization.asset", "Drone Stabilization Upgrade")]
+    public class DroneStabilizationUpgrade : DroneUpgrade
+    {
+        public float[] Stabilization => _stabilization;
+        
+        [Space] 
+        [SerializeField] private string _safeValueStr = "Стабильность: ";
+        [SerializeField] private float[] _stabilization;
+
+        protected override void OnView(int currentLevel, DroneUpgradeView view, bool isMax)
+        {
+            base.OnView(currentLevel, view);
+            if (isMax)
+                view.AddInfo(_safeValueStr, currentLevel.ToString());
+            else
+                view.AddInfo(_safeValueStr, 
+                    currentLevel.ToString(), (currentLevel + 1).ToString());
+        }
+    }
+}
