b32f1be981685cc82466595a8f30e386138c3cc4
Pacelin <Warfacer2020@gmail.com>
2025-04-12 19:11:34 +0300

    feat: DroneBars

---
+++ Assets/_Project/Scripts/Game/Drones/ArrowMeterDroneBar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using R3;
+using UnityEngine;
+
+namespace GGJam25.Game.Indicators
+{
+    public class ArrowMeterDroneBar : MonoBehaviour
+    {
+        [SerializeField] private Transform _origin;
+        [SerializeField] private float _rotation0;
+        [SerializeField] private float _rotationAmplitude;
+        
+        private IDisposable _dipsosable;
+        
+        public void Initialize(Observable<float> fill)
+        {
+            _dipsosable?.Dispose();
+            _dipsosable = fill.Subscribe(f =>
+            {
+                var fromRotation = _rotation0 - _rotationAmplitude;
+                var toRotation = _rotation0 + _rotationAmplitude;
+                var t = (f + 1) / 2f;
+                _origin.rotation = Quaternion.Euler(0, 0, t);
+            });
+        }
+
+        private void OnDestroy() => _dipsosable?.Dispose();
+    }
+}
--- Assets/_Project/Scripts/Game/Drones/DroneBar.cs
+++ Assets/_Project/Scripts/Game/Drones/DroneBar.cs
@@ -1,34 +1,10 @@
-﻿using System;
-using R3;
+﻿using R3;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace GGJam25.Game.Indicators
 {
-    public class DroneBar : MonoBehaviour
+    public abstract class DroneBar : MonoBehaviour
     {
-        [SerializeField] private Image _fillNegativeImage;
-        [SerializeField] private Image _fillPositiveImage;
-        [SerializeField] private float _multiplier = 1;
-        
-        private IDisposable _dipsosable;
-        
-        public void Initialize(Observable<float> fill)
-        {
-            _dipsosable?.Dispose();
-            _dipsosable = fill.Subscribe(f =>
-            {
-                float negative = 0;
-                float positive = 0;
-                if (f > 0)
-                    positive = f;
-                else
-                    negative = -f;
-                _fillNegativeImage.fillAmount = negative * _multiplier;
-                _fillPositiveImage.fillAmount = positive * _multiplier;
-            });
-        }
-
-        private void OnDestroy() => _dipsosable?.Dispose();
+        public abstract void Initialize(Observable<float> fill);
     }
 }
