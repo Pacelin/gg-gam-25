07e60e14f39fdc6c8947f99093390f994c9371fa
Сергей Командиров <grey7509@mail.ru>
2025-04-13 16:05:45 +1000

    fix
    f

--- Assets/_Project/Scripts/DangerFxMove/MotionTornado.cs
+++ Assets/_Project/Scripts/DangerFxMove/MotionTornado.cs
@@ -2,6 +2,44 @@ using UnityEngine;
 
 public class MotionTornado : MonoBehaviour
 {
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private float radiusOffset = 0f;
+
+    private Transform centerPoint;
+    private float currentAngle = 0f;
+    private float actualRadius;
+
+    void Start()
+    {
+        centerPoint = transform.parent;
+
+        if (centerPoint == null)
+        {
+            centerPoint = new GameObject("CenterPoint").transform;
+            centerPoint.position = transform.position;
+            transform.SetParent(centerPoint);
+        }
+
+        actualRadius = Vector3.Distance(transform.position, centerPoint.position) + radiusOffset;
+    }
+
+    void Update()
+    {
+        currentAngle += speed * Time.deltaTime;
+
+        float x = Mathf.Cos(currentAngle) * actualRadius;
+        float z = Mathf.Sin(currentAngle) * actualRadius;
+
+        transform.localPosition = new Vector3(x, transform.localPosition.y, z);
+    }
+
+    public void SetRadius(float newRadius)
+    {
+        actualRadius = newRadius + radiusOffset;
+    }
+}
+
+/*{
     [SerializeField] private float radius = 5f;    
     [SerializeField] private float speed = 2f;     
 
@@ -18,4 +56,4 @@ public class MotionTornado : MonoBehaviour
         
         transform.position = new Vector3(x, transform.position.y, z);
     }
-}
+}*/
