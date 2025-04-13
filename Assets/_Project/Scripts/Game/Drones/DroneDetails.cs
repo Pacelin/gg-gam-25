b02b27372bc12e285d3e380c733caf9c48dfac26
Pacelin <Warfacer2020@gmail.com>
2025-04-13 16:26:52 +0300

    feat: Drone Details

---
+++ Assets/_Project/Scripts/Game/Drones/DroneDetails.cs
@@ -0,0 +1,36 @@
+﻿using R3;
+using UnityEngine;
+
+namespace GGJam25.Game.Drones
+{
+    public class DroneDetails : MonoBehaviour
+    {
+        [SerializeField] private GameObject _speedComponent;
+        [SerializeField] private GameObject _coldComponent;
+        [SerializeField] private GameObject _hotComponent;
+        [SerializeField] private GameObject _waterComponent;
+        [SerializeField] private GameObject _sandComponent;
+
+        private CompositeDisposable _disposables;
+        
+        private void OnEnable()
+        {
+            _disposables = new CompositeDisposable();
+            GameContext.DroneUpgrades.SpeedLevel.Subscribe(l =>
+                _speedComponent.SetActive(l > 0)).AddTo(_disposables);
+            GameContext.DroneUpgrades.ColdUpgradeLevel.Subscribe(l =>
+                _coldComponent.SetActive(l > 0)).AddTo(_disposables);
+            GameContext.DroneUpgrades.HotUpgradeLevel.Subscribe(l =>
+                _hotComponent.SetActive(l > 0)).AddTo(_disposables);
+            GameContext.DroneUpgrades.WaterUpgradeLevel.Subscribe(l =>
+                _waterComponent.SetActive(l > 0)).AddTo(_disposables);
+            GameContext.DroneUpgrades.SandUpgradeLevel.Subscribe(l =>
+                _sandComponent.SetActive(l > 0)).AddTo(_disposables);
+        }
+
+        private void OnDisable()
+        {
+            _disposables.Dispose();
+        }
+    }
+}
